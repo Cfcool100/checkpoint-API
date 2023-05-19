673066fe85d276bbb0cd9a08badf515ddc4acd6b
Youssouf SISSOKOmrsissoko@Sissokos-MacBook-Pro ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-05-19 21:40:07 +0000

    v.1.2 : Mise à jour du composant UserList

--- src/UserList.js
+++ src/UserList.js
@@ -2,36 +2,38 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const UserList = () => {
-  const [listOfUser, setlistOfUser] = useState([]);
+    const [listOfUser, setlistOfUser] = useState([]);
 
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
-      console.log(res.data);
-      const posts = res.data;
-      setlistOfUser(posts);
-    });
-  }, []);
-  console.log(listOfUser);
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th className="">Name</th>
-          <th className="">User Name</th>
-          <th className="">Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        {listOfUser.map((user, index) => (
-          <tr key={index}>
-            <td className="data">{user.name}</td>
-            <td className="data">{user.username}</td>
-            <td className="data">{user.email}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((res) => {
+            const posts = res.data;
+            setlistOfUser(posts);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }, []);
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th className="">Name</th>
+                    <th className="">User Name</th>
+                    <th className="">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listOfUser.map((user, index) => (
+                    <tr key={user.id}>
+                        <td className="data">{user.name}</td>
+                        <td className="data">{user.username}</td>
+                        <td className="data">{user.email}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
 };
 
 export default UserList;
